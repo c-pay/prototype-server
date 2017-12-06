b10d842e506e71b9c71f5edd6e19bd990ea53d78
Roman <smolyakovroma@gmail.com>
2017-12-07 00:30:49 +0700

    Initial commit

---
+++ src/main/java/com/prototype/server/prototypeserver/PrototypeServerApplication.java
@@ -0,0 +1,12 @@
+package com.prototype.server.prototypeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeServerApplication.class, args);
+	}
+}
